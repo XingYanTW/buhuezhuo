95be56e14ec6a1efc9c106e3ec4febd3307b9e44
XingYan <paul2487rqww95@gmail.com>
2024-09-20 19:18:55 +0800

    Upload FIles

---
+++ Assets/Option/BackScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackScript : MonoBehaviour
+{
+    public void click(){
+        SceneManager.LoadScene("Main");    
+    }
+}
