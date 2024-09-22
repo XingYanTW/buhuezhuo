e96548183aaa773b09d37fa0e6b0f685c5600998
xy <paul2487rqww95@gmail.com>
2024-09-22 19:20:51 +0800

    Mute Button

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,13 +6,35 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
 
-    public Slider mainSlider;
+    public Slider audioSlider;
+	//public Toggle audioToggle;
+	public Button audioButton;
+	//public Image Checkmark;
+	public Sprite AudioOn;
+	public Sprite AudioOff;
+	private float audioVolume;
 
 
     // Invoked when the value of the slider changes.
 	public void ValueChangeCheck()
 	{
-		Debug.Log (mainSlider.value);
-        GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMScript>().ChangeVolume(mainSlider.value);
+		Debug.Log (audioSlider.value);
+        GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMScript>().ChangeVolume(audioSlider.value);
+		if(audioSlider.value == 0){
+			audioButton.GetComponent<Image>().sprite = AudioOff;
+		}else{
+			audioButton.GetComponent<Image>().sprite = AudioOn;
+		}
+	}
+
+	public void click(){
+		if (audioButton.GetComponent<Image>().sprite.Equals(AudioOn)){
+			audioVolume = audioSlider.value;
+			audioSlider.value = 0;
+			audioButton.GetComponent<Image>().sprite = AudioOff;
+		}else{
+			audioButton.GetComponent<Image>().sprite = AudioOn;
+			audioSlider.value = audioVolume;
+		}
 	}
 }
