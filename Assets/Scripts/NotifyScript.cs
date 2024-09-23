aa6a7c535bb816b176ef1782d2bb3e0655562f7a
xy <paul2487rqww95@gmail.com>
2024-09-23 08:03:06 +0800

    textures

---
+++ Assets/Scripts/NotifyScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotifyScript : MonoBehaviour
+{
+    private Animator anim;
+    public Image notify;
+    public void Colse(){
+        anim = notify.GetComponent<Animator>();
+        anim.Play("Base Layer.NotifyOff");
+    }
+}
