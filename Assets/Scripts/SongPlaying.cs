97acbf40f214bf7ae78af0202b32cd8e25076507
xy <paul2487rqww95@gmail.com>
2024-10-02 16:22:29 +0800

    Key Down Test

---
+++ Assets/Scripts/SongPlaying.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongPlaying : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Note_1;
+    public GameObject TargetNote_1;
+    private GameObject Note;
+    public float speed = 1f; // Speed at which the note moves
+
+    void Start()
+    {
+        CreateNote();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the note towards the target every frame
+        if (Note != null && TargetNote_1 != null)
+        {
+            Note.transform.position = Vector3.MoveTowards(Note.transform.position, TargetNote_1.transform.position, speed * Time.deltaTime);
+            if(Note.transform.position.Equals(TargetNote_1.transform.position)){
+                Destroy(Note);
+                CreateNote();
+            }
+        }
+    }
+
+    void CreateNote(){
+        GameObject Canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Note = Instantiate(Note_1);
+        Note.SetActive(true);
+        Note.transform.SetParent(Canvas.transform, false);
+    }
+}
