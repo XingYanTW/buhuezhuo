5e00e55a3df792c9dfc3e1124237c7e19dc2f5f3
xy <paul2487rqww95@gmail.com>
2024-12-09 21:01:25 +0800

    客家讀取動畫

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -1,18 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
 
     private static string CurrentSong;
 
+    public GameObject LoadingScreen, SongName, SongJacket;
+    private Animator loadingScreenAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        loadingScreenAnimator = LoadingScreen.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +27,20 @@ public class PlayButton : MonoBehaviour
     }
     public void PlaySong(){
         Debug.Log(CurrentSong);
+        LoadingScreen.SetActive(true);
+        loadingScreenAnimator.SetTrigger("SlideUp");
+
+        SongName.GetComponent<TextMeshProUGUI>().text = CurrentSong;
+        var _jacket = Resources.Load<Sprite>("Songs/"+CurrentSong+"/Jacket");
+        SongJacket.GetComponent<Image>().sprite = _jacket;
+        //SceneManager.LoadScene("SongPlaying");
+
+        //switch scene to SongPlaying after 3 seconds
+        StartCoroutine(LoadScene());
+    }
+    
+    IEnumerator LoadScene(){
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene("SongPlaying");
     }
 
